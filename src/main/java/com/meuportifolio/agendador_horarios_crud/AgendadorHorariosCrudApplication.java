93d0ae71a5fbb0f5df405f943d493c82f86aa7db
Carlos Vinícyus <carlosvinicyuss@gmail.com>
2026-02-23 21:36:53 -0300

    :tada: Commit inicial

---
+++ src/main/java/com/meuportifolio/agendador_horarios_crud/AgendadorHorariosCrudApplication.java
@@ -0,0 +1,13 @@
+package com.meuportifolio.agendador_horarios_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendadorHorariosCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendadorHorariosCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meuportifolio/agendador_horarios_crud/AgendadorHorariosCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meuportifolio.agendador_horarios_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorHorariosCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
